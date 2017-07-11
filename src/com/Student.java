023d9c5eb22f6396c51055d37fafbbafa2352ba2
jiarh <651841797@qq.com>
2017-07-11 21:23:52 +0800

    add parent

--- src/com/Student.java
+++ src/com/Student.java
@@ -7,6 +7,7 @@ public class Student {
     private String name;
     private String code;
     private Integer socre;
+    private String parent;
 
     public String getName() {
         return name;
