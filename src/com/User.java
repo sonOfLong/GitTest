a3b0a4e140cdedde0ad16f41ba08eb7d6aa55aec
jiarh <651841797@qq.com>
2017-07-11 20:28:44 +0800

    create a project

---
+++ src/com/User.java
@@ -0,0 +1,8 @@
+package com;
+
+/**
+ * Created by 65184 on 2017/7/11.
+ */
+public class User {
+    private String username;
+}
--- web/index.jsp
+++ web/index.jsp
@@ -11,6 +11,6 @@
     <title>$Title$</title>
   </head>
   <body>
-  $END$
+  this is a test
   </body>
 </html>
