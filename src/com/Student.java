9bfc1504857d538043bb8aa1007ae40cf22be29e
jiarh <651841797@qq.com>
2017-07-11 21:10:08 +0800

    add a Student.java

---
+++ src/com/Student.java
@@ -0,0 +1,34 @@
+package com;
+
+/**
+ * Created by 65184 on 2017/7/11.
+ */
+public class Student {
+    private String name;
+    private String code;
+    private Integer socre;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getSocre() {
+        return socre;
+    }
+
+    public void setSocre(Integer socre) {
+        this.socre = socre;
+    }
+}
